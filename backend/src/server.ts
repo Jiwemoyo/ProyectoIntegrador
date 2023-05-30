0f68fa72bbe914f21f10f9395146c817bdfb7550
Jinson25 <am987406@gmail.com>
2023-05-29 23:54:08 -0500

    rutas de la comidas routerfood

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { sample_foods, sample_tags, sample_users } from "./data";
 import jwt from "jsonwebtoken";
+import foodRouter from './routers/food.router'
 
 const app = express();
 app.use(express.json());
@@ -10,34 +11,7 @@ app.use(cors({
     origin:["http://localhost:4200"]
 }));
 
-app.get("/api/foods", (req, res) => {
-    res.send(sample_foods);
-})
-
-app.get("/api/foods/search/:searchTerm", (req, res) => {
-    const searchTerm = req.params.searchTerm;
-    const foods = sample_foods
-    .filter(food => food.name.toLowerCase()
-    .includes(searchTerm.toLowerCase()));
-    res.send(foods);
-})
-
-app.get("/api/foods/tags", (req, res) => {
-    res.send(sample_tags);
-})
-
-app.get("/api/foods/tag/:tagName", (req, res) => {
-    const tagName = req.params.tagName;
-    const foods = sample_foods
-    .filter(food => food.tags?.includes(tagName));
-    res.send(foods);
-})
-
-app.get("/api/foods/:foodId", (req, res) => {
-    const foodId = req.params.foodId;
-    const food = sample_foods.find(food => food.id == foodId);
-    res.send(food);
-})
+app.use("/api/foods" , foodRouter)
 
 app.post("/api/users/login", (req,res) => {
     const {email, password} = req.body;
